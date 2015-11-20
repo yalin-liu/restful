e944d7e6d1d56c7e038caabeaeb90bf91cc43be4
Raymond SO <rso@ouhk.edu.hk>
2015-11-20 12:31:55 +0800

    added path labels

--- requests/server.js
+++ requests/server.js
@@ -5,18 +5,21 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Path 1
 app.get('/name/:name', function(req,res) {
 	console.log('Incoming request: GET');
 	console.log('Request body: ', req.body);
 	console.log('name: ', req.params.name);
 });
 
+// Path 2
 app.get('/age/:age', function(req,res) {
 	console.log('Incoming request: GET');
 	console.log('Request body: ', req.body);
 	console.log('age: ', req.params.age);
 });
 
+// Path 3
 app.get('/name/:name/age/:age', function(req,res) {
 	console.log('Incoming request: GET');
 	console.log('Request body: ', req.body);
@@ -24,12 +27,14 @@ app.get('/name/:name/age/:age', function(req,res) {
 	console.log('age: ', req.params.age);
 });
 
+// Path 4
 app.delete('/name/:name', function(req,res) {
 	console.log('Incoming request: DELETE');
 	console.log('Request body: ', req.body);
 	console.log('name: ', req.params.name);
 });
 
+// Path 5
 app.post('/',function(req,res) {
 	console.log('Incoming request: POST');
 	console.log('Request body: ', req.body);
@@ -38,6 +43,7 @@ app.post('/',function(req,res) {
 	res.sendStatus(200);
 });
 
+// Path 6
 app.put('/',function(req,res) {
 	console.log('Incoming request: PUT');
 	console.log('Request body: ', req.body);
