42d80fac03c33e284b5ce1d22f766dc204f05cac
Raymond SO <rso@ouhk.edu.hk>
2015-11-20 13:36:44 +0800

    fixed res return

--- requests/server.js
+++ requests/server.js
@@ -5,11 +5,20 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Path 0
+app.get('/', function(req,res) {
+	console.log('Incoming request: GET');
+	console.log('Request body: ', req.body);
+	console.log('name: ', req.params.name);
+	res.end('Connection closed',200);
+});
+
 // Path 1
 app.get('/name/:name', function(req,res) {
 	console.log('Incoming request: GET');
 	console.log('Request body: ', req.body);
 	console.log('name: ', req.params.name);
+	res.end('Connection closed',200);
 });
 
 // Path 2
@@ -17,6 +26,7 @@ app.get('/age/:age', function(req,res) {
 	console.log('Incoming request: GET');
 	console.log('Request body: ', req.body);
 	console.log('age: ', req.params.age);
+	res.end('Connection closed',200);
 });
 
 // Path 3
@@ -25,6 +35,7 @@ app.get('/name/:name/age/:age', function(req,res) {
 	console.log('Request body: ', req.body);
 	console.log('name: ', req.params.name);
 	console.log('age: ', req.params.age);
+	res.end('Connection closed',200);
 });
 
 // Path 4
@@ -32,6 +43,7 @@ app.delete('/name/:name', function(req,res) {
 	console.log('Incoming request: DELETE');
 	console.log('Request body: ', req.body);
 	console.log('name: ', req.params.name);
+	res.end('Connection closed',200);
 });
 
 // Path 5
@@ -40,7 +52,7 @@ app.post('/',function(req,res) {
 	console.log('Request body: ', req.body);
 	console.log('name: ', req.body.name);
 	console.log('age: ', req.body.age);
-	res.sendStatus(200);
+	res.end('Connection closed',200);
 });
 
 // Path 6
@@ -49,7 +61,7 @@ app.put('/',function(req,res) {
 	console.log('Request body: ', req.body);
 	console.log('name: ', req.body.name);
 	console.log('age: ', req.body.age);
-	res.sendStatus(200);
+	res.end('Connection closed',200);
 });
 
 app.listen(process.env.PORT || 8099);
