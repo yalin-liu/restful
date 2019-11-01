cfa9ae386ccf91d9346d6e589763858874c028a2
Raymond So <rso@ouhk.edu.hk>
2019-11-01 14:05:55 +0800

    used more ES6 syntax

--- simpleinterest/server.js
+++ simpleinterest/server.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.set('view engine', 'ejs');
 
 // curl localhost:8099/simpleinterest/10000/0.01/10"
 // curl --header "accept:application/json" "localhost:8099/simpleinterest/10000/0.01/10"
 app.get('/simpleinterest/:principal/:rate/:time', function(req,res) {
-	var r = new simpleInterest(Number(req.params.principal), Number(req.params.rate), Number(req.params.time));
-	console.log(JSON.stringify(r));
+	let r = new SimpleInterest(Number(req.params.principal), Number(req.params.rate), Number(req.params.time));
 	if (req.headers['accept'] == 'application/json') {		
 		res.status(200).json(r);
 	} else {
@@ -15,11 +14,13 @@ app.get('/simpleinterest/:principal/:rate/:time', function(req,res) {
 	}
 });
 
-function simpleInterest(p,r,t) {
-	this.principal = p;
-	this.rate = r;
-	this.time = t;
-	this.interest = p * r * t;
+class SimpleInterest {
+	constructor(p,r,t) {
+		this.principal = p;
+		this.rate = r;
+		this.time = t;
+		this.interest = p * r * t;
+	}
 }
 
 app.listen(process.env.PORT || 8099);
