054fe69264c0bd0aca0fcf1795aa8a7f2891fa7d
Raymond SO <rso@ouhk.edu.hk>
2019-11-15 21:42:03 +0800

    updated - now using EJS

--- shape/server.js
+++ shape/server.js
@@ -3,15 +3,26 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+/*
 app.get('/api/square/area/:length', (req,res) => {
 	let result = {};
 	result.length = req.params.length;
 	result.area = result.length ** 2;
 	res.status(200).json(result);
 });
+*/
 
 app.get('/square/area/:length', (req,res) => {
-	res.status(200).end(`Area of square having length ${req.params.length} = ${req.params.length ** 2}`);
+	var result = {};
+	result.shape = 'square';
+	result.length = req.params.length;
+	result.area = result.length ** 2;
+	console.log(req.headers.accept);
+	if (req.headers.accept == 'application/json') {
+		res.status(200).json(result);
+	} else {
+		res.status(200).render('answer.ejs',{answer: result});
+	}
 })
 
 // curl --header "accept:application/json" "localhost:8099/api/square/area/17"
